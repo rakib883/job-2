80204f04f08f1c869954b102f5786a4ba767689e
rakib883 <sheikhrakib883@gmail.com>
2024-11-14 01:57:03 +0600

    last

--- src/component/Budget.jsx
+++ src/component/Budget.jsx
@@ -11,15 +11,15 @@ const Budget = () => {
         <div className="item py-[40px]">
            <Container className="mx-auto">
               <div className="item flex">
-                 <div className="text-area w-[50%]  mt-[40px]">
+                 <div className="text-area lg:w-[50%] mobile:w-full  mt-[40px]">
                     <div className="top">
                         <p className="font-inter font-medium text-[14px]">Get In Touch</p>
                     </div>
-                    <div className="title leading-[40px] max-w-[512px] mt-[16px]">
-                        <Title className="font-bold text-[32px] font-inter text-black" title="From Comfort to Budget, Explore Ticket Options for Every Traveler" />
+                    <div className="title max-w-[512px] mt-[16px]">
+                        <Title className="font-bold lg:text-[32px] font-inter text-black  mobile:text-[16px]  lg:leading-[40px] mobile:leading-[20px]" title="From Comfort to Budget, Explore Ticket Options for Every Traveler" />
                     </div>
-                    <p className="font-inter font-medium text-[14px] mt-[16px]">Seamless booking, diverse routes, and flexible schedules at your fingertips</p>
-                    <div className="social mt-[40px]">
+                    <p className="font-inter font-medium lg:text-[14px] lg:mt-[16px] mobile:mt-[8px]">Seamless booking, diverse routes, and flexible schedules at your fingertips</p>
+                    <div className="social lg:mt-[40px] mobile:mt-[24px]">
                         <div className="item">
                             <div className="phone flex items-center gap-4 font-inter text-[16px] font-medium">
                                <p><FiPhone className="text-[16px]" /></p>
@@ -36,8 +36,8 @@ const Budget = () => {
                         </div>
                     </div>
                  </div>
-                 <div className="galle w-[50%]">
-                    <div className="item flex gap-0 items-center">
+                 <div className="galle w-[50%] mobile:hidden lg:block">
+                    <div className="item flex gap-0 items-center ">
                           <div className="first">
                               <img className="w-[212px] h-[420px]" src="https://i.ibb.co.com/ZLqvHW1/Bus-tour-1.png" alt="img" />
                           </div>
